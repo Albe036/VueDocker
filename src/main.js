4c6a80de31c7483db14b2915e3e82b01bdf0bc18
Albeiro Lozano <Albeiro036@gmail.com>
2023-11-27 00:01:08 -0500

    added docker-compose

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import { createVuetify } from 'vuetify'
+
 import './style.css'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives
+})
+
+app.use(vuetify)
+
+app.mount('#app')
